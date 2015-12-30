62fbea2d4018f904a9a35f138522394d6dacd8e0
Tony Bowden <tony@tmtm.com>
2015-12-30 16:37:34 +0000

    New version of EveryPolitician::Wikidata

--- scraper.rb
+++ scraper.rb
@@ -8,36 +8,64 @@ require 'scraperwiki'
 require 'wikidata/fetcher'
 require 'mediawiki_api'
 
+module EveryPolitician
+  
+  module Wikidata
 
-def en_members
-  morph_api_key = ENV["MORPH_API_KEY"]
-  en_api_url = 'https://api.morph.io/tmtmtmtm/albania-assembly-wp/data.json'
-  en = JSON.parse(RestClient.get(en_api_url, params: {
-    key: morph_api_key,
-    query: "select DISTINCT(wikiname) AS wikiname from data"
-  }), symbolize_names: true).map { |n| n[:wikiname] }
-  return WikiData.ids_from_pages('en', en)
-end
+    require 'json'
+    require 'rest-client'
 
-def al_members
-  morph_api_key = ENV["MORPH_API_KEY"]
-  al_api_url = 'https://api.morph.io/tmtmtmtm/albania-kuvendi-wikipedia/data.json'
-  al = JSON.parse(RestClient.get(al_api_url, params: {
-    key: morph_api_key,
-    query: "select DISTINCT(wikiname) AS wikiname from data"
-  }), symbolize_names: true).map { |n| n[:wikiname] }
-  return WikiData.ids_from_pages('sq', al)
-end
+    def self.morph_wikinames(h)
+      morph_api_url = 'https://api.morph.io/%s/data.json' % h[:source]
+      morph_api_key = ENV["MORPH_API_KEY"]
+      result = RestClient.get morph_api_url, params: {
+        key: morph_api_key,
+        query: "SELECT DISTINCT(#{h[:column]}) AS wikiname FROM data"
+      }
+      JSON.parse(result, symbolize_names: true).map { |h| h[:wikiname] }.compact.reject(&:empty?)
+    end
 
-(en_members.values + al_members.values).uniq.each do |wid|
-  data = WikiData::Fetcher.new(id: wid).data('en', 'sq') rescue nil
-  unless data
-    warn "No data for #{wid}"
-    next
+    #-------------------------------------------------------------------
+
+    require 'wikidata/fetcher'
+    require 'scraperwiki'
+
+    def self.scrape_wikidata(h)
+      langs = ((h[:lang] || h[:names].keys) + [:en]).flatten.uniq
+      langpairs = h[:names].map { |lang, names| WikiData.ids_from_pages(lang.to_s, names) }
+      combined  = langpairs.reduce({}) { |h, people| h.merge(people.invert) }
+
+      combined.each do |id, name|
+        data = WikiData::Fetcher.new(id: id).data(langs) rescue nil
+        unless data
+          warn "No data for #{id}"
+          next
+        end
+        data[:original_wikiname] = name
+        warn data
+        ScraperWiki.save_sqlite([:id], data)
+      end
+
+    end
+
+    #-------------------------------------------------------------------
+
+    require 'rest-client'
+
+    def self.notify_rebuilder
+      RestClient.post ENV['MORPH_REBUILDER_URL'], {} if ENV['MORPH_REBUILDER_URL']
+    end
   end
-  # TODO check if there's been a redirect
-  ScraperWiki.save_sqlite([:id], data)
 end
+
+
+
+names_en = EveryPolitician::Wikidata.morph_wikinames(source: 'tmtmtmtm/albania-kuvendi-wikipedia', column: 'wikiname__en')
+names_sq = EveryPolitician::Wikidata.morph_wikinames(source: 'tmtmtmtm/albania-kuvendi-wikipedia', column: 'wikiname__sq')
+EveryPolitician::Wikidata.scrape_wikidata(names: { 
+  en: names_en,
+  sq: names_sq,
+})
 
 require 'rest-client'
 warn RestClient.post ENV['MORPH_REBUILDER_URL'], {} if ENV['MORPH_REBUILDER_URL']
